e43a17aa998bb7f673c3dbd10ce5bb8cbdb16328
Federico Shelna <fshelna@c1r3p7.42firenze.it>
2026-01-27 11:53:48 +0100

    eos

--- mod3/ex0/ft_command_quest.py
+++ mod3/ex0/ft_command_quest.py
@@ -8,13 +8,7 @@ if __name__ == "__main__":
 
     if av_len - 1 == 0:
         print("No arguments provided!")
-
-    for c in sys.argv[0]:
-        lst = lst + [c]
-    for e in lst:
-        print("element: " + e)
     print(f"Program name: {sys.argv[0]}")
-    
     if av_len - 1 > 0:
         print(f"Arguments received: {av_len - 1}")
         while index < av_len:
--- mod3/ex1/ft_score_analytics.py
+++ mod3/ex1/ft_score_analytics.py
@@ -1,5 +1,6 @@
 import sys
 
 if __name__ == "__main__":
+    print("=== Player Score Analytics ===\n")
     
     pass 
