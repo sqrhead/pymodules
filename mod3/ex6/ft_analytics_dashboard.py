eff2ea94c085d4aa7bc04b8dd1ed234d5c96c795
Federico Shelna <fshelna@c1r3p7.42firenze.it>
2026-01-30 12:04:14 +0100

    eos

--- mod3/ex6/ft_analytics_dashboard.py
+++ mod3/ex6/ft_analytics_dashboard.py
@@ -3,24 +3,24 @@ if __name__ == "__main__":
     print("=== Game Analytics Dashboard ===\n")
 
     print("=== List Comprehension Examples ===")
-    lst: list = ["alice", "charlie", "diana", "bob"]
-    lst_2000 = [x for x in lst if x != "bob"]
-    scores = [2300, 1800, 2150, 2050]
-    doubled = [y * 2 for y in scores]
-    active = [j for j in lst if j != "diana"]
+    
+    users = [
+        {"name":"alice", "score":2300, "active":True, "achievements": []},
+        {"name":"charlie", "score":1800, "active":True, "achievements": []},
+        {"name":"diana", "score":2150, "active":False, "achievements": []},
+        {"name":"bob", "score":2050, "active":True, "achievements": []},
+    ]
+    lst_2000 = [x["name"] for x in users if x["score"] > 2000]
+    doubled = [y["score"] * 2 for y in users]
+    active = [j["name"] for j in users if j["active"] is True]
 
     print(f"High scorers (>2000): {lst_2000}")
     print(f"Scores doubled: {doubled}")
     print(f"Active players: {active}\n")
 
     print("=== Dict Comprehension Examples ===")
-    player_scores: dict = {
-        'alice':2300,
-        'bob': 1800,
-        'charlie': 2150,
-        'diana': 2050
-    }
-
+    active_player_scores: dict = {k["name"]:v["score"] for (k,v) in users if k["active"] is True}
+    print(active_player_scores)
     print("=== Set Comprehension Examples ===")
 
     print("=== Combined Analysis ===")
