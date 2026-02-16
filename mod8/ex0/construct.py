aa2652d8260f973b2f478607a11a1523be13407a
sqrhead <fshelna98@gmail.com>
2026-02-16 23:21:20 +0100

    add_cmm

--- mod8/ex0/construct.py
+++ mod8/ex0/construct.py
@@ -1,9 +1,16 @@
 import sys
 
+# Authorized
+# sys, os, site modules, print()
+
 if __name__ == "__main__":
 
     # check if inside virtual env
     if sys.prefix != sys.base_prefix:
+        print("MATRIX STATUS: You're still plugged in")
+        # Current Python: /usr/bin/python3.11 -> current python interpreter used
+        # Virtual Environment: None detected -> folder where venv is
+        # /path/to/matrix_env/lib/python3.11/site-packages -> where packages are installed
         print("INSIDE VENV")
         print(f"{sys.prefix}")
     else:
