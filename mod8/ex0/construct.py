7252b5c917b9240f9c921269cfd943e7f5dcea85
Federico Shelna <fshelna@c1r3p7.42firenze.it>
2026-02-17 12:34:05 +0100

    mod

--- mod8/ex0/construct.py
+++ mod8/ex0/construct.py
@@ -1,18 +1,42 @@
 import sys
-
+import os 
 # Authorized
 # sys, os, site modules, print()
 
 if __name__ == "__main__":
 
     # check if inside virtual env
-    if sys.prefix != sys.base_prefix:
-        print("MATRIX STATUS: You're still plugged in")
+    if 'VIRTUAL_ENV' in os.environ: # List of enviroment variables 
+        # print("MATRIX STATUS: You're still plugged in")
         # Current Python: /usr/bin/python3.11 -> current python interpreter used
         # Virtual Environment: None detected -> folder where venv is
         # /path/to/matrix_env/lib/python3.11/site-packages -> where packages are installed
-        print("INSIDE VENV")
-        print(f"{sys.prefix}")
+        print("=== INSIDE VENV ===")
+        print("MATRIX STATUS: Welcome to the construct")
+        print(f"\nCurrent python: {sys.executable}")
+        print(f"Virtual Enviroment: {os.name}")
+        print(f"Enviroment path: {os.environ['VIRTUAL_ENV']}")
+        
+        print("\nSUCCESS: You're in an isolated environment!")
+        print("Safe to install packages without affecting the global system.")
+
+        print(f"\nPackage installation path: \n{...}")
+        # print(f"{sys.executable}") # python interpreter location 
+        # print(f"{os.environ['VIRTUAL_ENV']}") # venv location
     else:
-        print("OUTSIDE VENV")
-        print(f"{sys.prefix}")
+        print("=== OUTSIDE VENV ===")
+        print("MATRIX STATUS: You're still plugged in")
+
+        print(f"\nCurrent python : {sys.executable}")
+        print("Virtual Environment: None detected")
+
+        print("\nWARNING: You're in the global environment!")
+        print("The machines can see everything you install.")
+
+        print("\nTo enter the construct, run:")
+        print("python -m venv matrix_env")
+        print("source matrix_env/bin/activate # On Unix")
+        print("matrix_venv\nScripts\nactivate # On Windows")
+        print("\nThen run this program again")
+        # print(f"{os.environ['VIRTUAL_ENV']}")
+
